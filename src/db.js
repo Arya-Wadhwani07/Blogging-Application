a6caab079590816eaa88070e91306370793ae4b7
Arya-Wadhwani07 <arya.wadhwani@gmail.com>
2022-01-14 03:16:42 +0530

    Refactor index.js into directory structure

---
+++ src/db.js
@@ -0,0 +1,66 @@
+const users = [{
+    id: '1',
+    name: 'Arya',
+    email: 'arya@example.com',
+    age: 19,
+}, {
+    id: '2',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27,
+}, {
+    id: '3',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}]
+
+const posts = [{
+    id: '1',
+    title: 'Course1',
+    body: 'This is course 1',
+    published: false,
+    author: '1'
+}, {
+    id: '2',
+    title: 'Course2',
+    body: 'This is course 2',
+    published: true,
+    author: '1'
+}, {
+    id: '3',
+    title: 'Course3',
+    body: 'This is course 3',
+    published: false,
+    author: '2'
+}]
+
+const comments = [{
+    id: '1',
+    text: 'This is the first comment',
+    author: '1',
+    post: '1'
+}, {
+    id: '2',
+    text: 'This is the second comment',
+    author: '1',
+    post: '2'
+}, {
+    id: '3',
+    text: 'This is the third comment',
+    author: '2',
+    post: '3'
+}, {
+    id: '4',
+    text: 'This is the fourth comment',
+    author: '3',
+    post: '1'
+}]
+
+const db = {
+    users,
+    posts,
+    comments
+}
+
+export { db as default }
+
---
+++ src/resolvers/Query.js
@@ -0,0 +1,44 @@
+const Query = {
+    users(parent, args, { db }, info) {
+        if (!(args.query)) {
+            return db.users
+        }
+
+        return db.users.filter((user) => {
+            return user.name.toLowerCase().includes(args.query.toLowerCase())
+        })
+    },
+    posts(parent, args, { db }, info) {
+        if (!(args.query)) {
+            return db.posts
+        }
+
+        return db.posts.filter((post) => {
+            if (post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())) {
+                return true
+            }
+            return false
+        })
+    },
+    me() {
+        return {
+            id: 'abcd1234',
+            name: 'Mike Hannigan',
+            email: 'mike@example.com',
+            age: 27
+        }
+    },
+    post() {
+        return {
+            id: 'abcdef123456',
+            title: 'The Subtle Art of Greatness',
+            body: 'I am the killer who is born to be great',
+            published: false
+        }
+    },
+    comments(parent, args, { db }, info) {
+        return db.comments
+    }
+}
+
+export { Query as default }
